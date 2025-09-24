1970ca5d89c2f92c4e959c0a8ef1fa6d2fedd58b
Mark <dodlepcgaming@gmail.com>
2025-09-24 12:19:39 -0400

    PHP user agent added
    agent

--- DASITE/index.php
+++ DASITE/index.php
@@ -2,5 +2,10 @@
 <html lang=en>
     <body>
         <p><?= var_dump($_SERVER) ?></p>
+
+        <?php
+            $userAgent = $_SERVER['HTTP_USER_AGENT'];
+            echo "Your User Agent: " . $userAgent;
+        ?>
     </body>
 </html>
