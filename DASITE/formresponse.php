6bdf236c3e30fd2990e4ec64989578cb576097b9
Mark Drobyshev <dodlepcgaming@gmail.com>
2025-09-25 19:34:46 -0400

    form response using get

---
+++ DASITE/formresponse.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Response</title>
+</head>
+<body>
+    <h2>Raw Data</h2>
+    <p>GET: <?php var_dump($_GET); ?></p>
+    <p>POST: <?php var_dump($_POST); ?></p>
+</body>
+</html>
