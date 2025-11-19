3fcab162119d88ba3566a3f902b608525a0486c4
Mark <dodlepcgaming@gmail.com>
2025-11-19 14:42:35 -0500

    fix led?

--- LED.php
+++ LED.php
@@ -1,37 +1,35 @@
 <?php
+// Ensure pin is set to output mode
+shell_exec("gpio mode 7 out");
+
 // Read current LED state
 $currentState = trim(shell_exec("gpio read 7"));
 
-// Handle button press
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['toggle'])) {
-        // Toggle the LED
         shell_exec("gpio toggle 7");
         $currentState = trim(shell_exec("gpio read 7"));
     }
     if (isset($_POST['button'])) {
-        // Set LED ON or OFF based on current state
         if ($currentState == "1") {
-            shell_exec("gpio write 7 0");
+            shell_exec("gpio write 7 0"); // turn OFF
         } else {
-            shell_exec("gpio write 7 1");
+            shell_exec("gpio write 7 1"); // turn ON
         }
         $currentState = trim(shell_exec("gpio read 7"));
     }
 }
-$buttonlabel1 = "Toggle LED";
-$buttonlabel2 = ($currentState == "1") ? "Turn ON" : "Turn OFF";
-?>
 
+$buttonlabel1 = "Toggle LED";
+$buttonlabel2 = ($currentState == "1") ? "Turn OFF" : "Turn ON";
+?>
 <html>
-<head>
-    <title>LED Control</title>
-</head>
+<head><title>LED Control</title></head>
 <body>
 <h1>LED BUTTONS</h1>
 <form method="post">
-    <input type="submit" name="toggle" value="1"><?php echo $buttonlabel1; ?>
-    <input type="submit" name="button" value="1"><?php echo $buttonlabel2; ?>
+    <input type="submit" name="toggle" value="<?php echo $buttonlabel1; ?>">
+    <input type="submit" name="button" value="<?php echo $buttonlabel2; ?>">
 </form>
 <p>LED State: <?php echo htmlspecialchars($currentState); ?></p>
 </body>
